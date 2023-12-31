75c7d774532781682d0505e275e66ab0041d12eb
Miguel Ángel Suvires <suvires@gmail.com>
2023-12-31 14:51:52 +0100

    Added start and stop recording function

--- src/components/Room/ControlBar.js
+++ src/components/Room/ControlBar.js
@@ -6,10 +6,10 @@ import {
 import './ControlBar.css'
 
 const ControlBar = () => {
-  const apiUrl = 'https://api.100ms.live'
-  const roomId = '6586b8fe3412d193e842030a'
-  const meetingUrl = `https://miguelngel-webinar-1123.app.100ms.live/preview/${roomId}/broadcaster?skip_preview=true`
-  const mangementToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDQwMjQyMzgsImV4cCI6MTcwNDExMDYzOCwianRpIjoiand0X25vbmNlIiwidHlwZSI6Im1hbmFnZW1lbnQiLCJ2ZXJzaW9uIjoyLCJuYmYiOjE3MDQwMjQyMzgsImFjY2Vzc19rZXkiOiI2NTg2YjQ2ZDkyOTYxM2FkNTdiNjUwYjAifQ.Sd8PHe8cu6RrgQM0IMGoutohxP0WOpsoDO3K_8MZTOc'
+  // const apiUrl = 'https://api.100ms.live'
+  // const roomId = '6586b8fe3412d193e842030a'
+  // const meetingUrl = `https://miguelngel-webinar-1123.app.100ms.live/preview/${roomId}/broadcaster?skip_preview=true`
+  // const mangementToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDQwMjQyMzgsImV4cCI6MTcwNDExMDYzOCwianRpIjoiand0X25vbmNlIiwidHlwZSI6Im1hbmFnZW1lbnQiLCJ2ZXJzaW9uIjoyLCJuYmYiOjE3MDQwMjQyMzgsImFjY2Vzc19rZXkiOiI2NTg2YjQ2ZDkyOTYxM2FkNTdiNjUwYjAifQ.Sd8PHe8cu6RrgQM0IMGoutohxP0WOpsoDO3K_8MZTOc'
 
   const {
     isLocalAudioEnabled,
@@ -25,35 +25,26 @@ const ControlBar = () => {
 
   const [isRecording, setIsRecording] = useState(false)
 
+  const serverUrl = process.env.REACT_APP_SERVER_URL // Cambia esto por la URL de tu servidor
+
   const startRecording = async () => {
     try {
-      const response = await fetch(`${apiUrl}/v2/recordings/room/${roomId}/start`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${mangementToken}`
-        },
-        body: JSON.stringify({
-          meeting_url: meetingUrl,
-          resolution: { width: 1280, height: 720 }
-        })
+      const response = await fetch(`${serverUrl}/start-recording`, {
+        method: 'POST'
+        // No necesitas enviar headers ni body, ya que tu servidor manejará esos detalles
       })
       const data = await response.json()
       console.log(data)
       setIsRecording(data.isRecording)
     } catch (error) {
-      console.error('Error startign recording:', error)
+      console.error('Error starting recording:', error)
     }
   }
 
   const stopRecording = async () => {
     try {
-      const response = await fetch(`${apiUrl}/v2/recordings/room/${roomId}/stop`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${mangementToken}`
-        }
+      const response = await fetch(`${serverUrl}/stop-recording`, {
+        method: 'POST'
       })
       const data = await response.json()
       console.log(data)
@@ -75,9 +66,6 @@ const ControlBar = () => {
       </button>
       <button className="btn-control" onClick={isRecording ? stopRecording : startRecording}>
         {isRecording ? 'Stop Recording' : 'Start Recording'}
-      </button>
-      <button className="btn-control" onClick={stopRecording}>
-        Stop Recording
       </button>
     </div>
   )
